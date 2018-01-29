f05c1e9ea050584b2c00dc4a91ede881df9310e1
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-01-29 12:42:57 +0900

    manual deploy

---
+++ js/tsuru.js
@@ -0,0 +1,18 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+for (var img of document.getElementsByClassName('gallery')){
+  var modalImg = document.getElementById("modal-img");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+  }
+}
+
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function() {
+  modal.style.display = "none";
+}
